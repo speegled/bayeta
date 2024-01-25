1f64c77fe00c2b0b422b3f2d1341e82e36dbbe6b
Darrin Speegle <speegled@slu.edu>
2024-01-25 09:27:43 -0600

    stoncks

--- data-raw/apple/data_creation.R
+++ data-raw/apple/data_creation.R
@@ -8,11 +8,25 @@ quantmod::getSymbols(Symbols = "GSPC", src = "google")
 aapl <- tidyquant::tq_get("AAPL", from = date)
 getSymbols('AAPL')
 library(lubridate)
-aapl <- aapl %>%
-  filter(month(date) == 1) %>%
-  mutate(year = year(date)) %>%
-  group_by(year) %>%
-  slice_min(day(date))
 
-aapl$percent_change <- c(NA, diff(aapl$adjusted)/aapl$adjusted[-1])
+mod_df <- function(aapl) {
+  aapl <- aapl %>%
+    filter(month(date) == 1) %>%
+    mutate(year = year(date)) %>%
+    group_by(year) %>%
+    slice_min(day(date))
+
+  aapl$percent_change <- c(NA, diff(aapl$adjusted)/aapl$adjusted[-1])
+  aapl
+}
+mod_df(aapl)
 write.csv(aapl, "data-raw/apple/apple.csv", row.names = F)
+
+
+wmt <- tidyquant::tq_get("WMT", from = date)
+wmt <- mod_df(wmt)
+mcd <- tidyquant::tq_get("MCD", from = date)
+mcd <- mod_df(mcd)
+
+three_stocks <- bind_rows(aapl, wmt, mcd)
+write.csv(three_stocks, "data-raw/apple/three_stocks.csv", row.names = F)
